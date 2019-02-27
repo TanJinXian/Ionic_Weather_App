093445c951d4d01d228cd39263a8270d3277a881
Jin Xian <jinxian@Jins-MacBook-Air.local>
2019-02-28 05:54:18 +0800

    complete

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,28 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  weatherInfo: any = {};
+  cityName: string = "London";
 
+  constructor(public navCtrl: NavController, public http: Http, public loadingCtrl: LoadingController) {
+    this.getData();
+  }
+
+  getData() {
+    let loading = this.loadingCtrl.create({
+      content: "Loading"
+    });
+
+    loading.present();
+    this.http.get("http://api.openweathermap.org/data/2.5/weather?q=" + this.cityName 
+    + "&APPID=315f056decbeefbde717e2865f08b542").subscribe((data) => {
+      console.log(data.json());
+      this.weatherInfo = data.json();
+      loading.dismiss();
+    },
+    (error) => {
+      console.log(error);
+    });
   }
 
 }
